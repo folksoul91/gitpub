03773b14b79dd9f0f0e3ad9b0cdf2517147b68cf
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-20 20:39:50 -0400

    adding a logic in index route to capitalize the first letter of the drink menu

--- server.js
+++ server.js
@@ -18,7 +18,27 @@ app.get("/", (req, res) => {
 });
 // Index - get drinks
 app.get("/pub", (req, res) => {
-  res.render("index.ejs", { drinks: drinks, food: food });
+  // loop through each drink in the database in order to properly capitalize them all
+  for (let drink of drinks) {
+    // split name into every separate word
+    let splitName = drink.name.split(" ");
+    // loop through each individual word
+    for (let i = 0; i < splitName.length; i++) {
+      // split the word into its individual letters
+      let splitWord = splitName[i].split("");
+      // uppercase the first letter
+      splitWord[0] = splitWord[0].toUpperCase();
+      // rejoin the word
+      splitName[i] = splitWord.join("");
+    }
+    // rejoin the words
+    drink.name = splitName.join(" ");
+  }
+  // rendering the index
+  res.render("index.ejs", {
+    drinks: drinks,
+    food: food,
+  });
 });
 // Config
 app.listen(PORT, () => {
